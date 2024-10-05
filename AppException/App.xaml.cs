4c65f142b271971bc0edfea6636ea2e408a80b80
te9yie <te9yie@users.noreply.github.com>
2024-10-05 20:14:56 +0900

    Add AppException project

---
+++ AppException/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AppException
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+            e.Handled = true;
+        }
+    }
+
+}
