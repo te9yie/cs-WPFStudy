dd553d753bcbf8e896505a2422de23484f0050c3
te9yie <te9yie@users.noreply.github.com>
2024-10-07 20:51:42 +0900

    Add PropertyChanged project

---
+++ PropertyChange/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyChange
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    NotifyPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            users.Add(new User() { Name = "Taro" });
+            users.Add(new User() { Name = "Jiro" });
+
+            listUsers.ItemsSource = users;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "New User" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (listUsers.SelectedItem is User user)
+            {
+                user.Name = "Change";
+            }
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (listUsers.SelectedItem is User user)
+            {
+                users.Remove(user);
+            }
+        }
+    }
+}
