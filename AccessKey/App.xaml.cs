9a121c1f9bda62ff941799b399769b02c7a8c69b
te9yie <te9yie@users.noreply.github.com>
2024-10-07 09:12:30 +0900

    Add AccessKey project

---
+++ AccessKey/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AccessKey
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
